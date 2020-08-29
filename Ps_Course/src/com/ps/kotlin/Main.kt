bd2bda195e65e272e61c1f84f464621496a61bbc
nightcrawler <singhajay9009@gmail.com>
2020-08-29 08:57:50 +0530

    Kotlin adding some programs

--- Ps_Course/src/com/ps/kotlin/Main.kt
+++ Ps_Course/src/com/ps/kotlin/Main.kt
@@ -10,4 +10,14 @@ fun main (args : Array<String>){
 
     var clssWithConstructor = ClassWithConstructor("black")
     println("prop is ${clssWithConstructor.prop}")
+
+
+
+
+    person.displayWithLambda (::printName)
+}
+
+
+fun printName(name: String): Unit{
+    println("Name is ${name}")
 }
--- Ps_Course/src/com/ps/kotlin/Person.kt
+++ Ps_Course/src/com/ps/kotlin/Person.kt
@@ -3,4 +3,8 @@ package com.ps.kotlin
 class Person {
 
     var name: String = "John"
+
+    fun displayWithLambda(func: (s: String) -> Unit){
+        func(name)
+    }
 }
